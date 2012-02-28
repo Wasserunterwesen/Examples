c6922bfcef55dd78881de6da9c9c53f985a3785f
Konstantin Paul <konsti.paul@googlemail.com>
2012-02-28 15:38:35 +0100

    Singelton Example
    A Singelton is a class which only can have one instance.

---
+++ singleton.php
@@ -0,0 +1,21 @@
+<?php
+
+error_reporting(E_ALL | E_STRICT);
+
+class Singleton {
+    private static $instance;
+
+    private function __construct(){}
+
+    public static function getInstance() {
+        if(self::$instance == null) {
+            self::$instance = new Singleton();
+        }
+
+        return self::$instance;
+    }
+    
+}
+
+$a = Singleton::getInstance();
+$b = Singleton::getInstance();
